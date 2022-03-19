e9e09c38115d337585e2c3de77674eb017856859
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-03-19 16:15:01 -0400

    added ability to add id numbers to notes

---
+++ server.js
@@ -0,0 +1,49 @@
+const PORT = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+var notes = require("./Develop/db/db.json");
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// starting the index html
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+// linking the notes page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
+// Calling in all posted notes
+app.get('/api/notes', (req, res) => {
+    
+    res.json(notes);
+});
+
+// Posting new notes
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    notes.push(newNote);
+    let num = 1
+    notes.forEach((note) => {
+        note.id = num;
+        num++;
+        return notes;
+    })
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes));
+    res.json(newNote);
+});
+
+// Deleting notes
+app.delete("/api/notes/:id", (req, res) => {
+
+})
+
+app.listen(PORT, e => console.log("Running"))
